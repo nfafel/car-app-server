0ad4b10e591ba0013ad26032f2781db22112a6f3
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-06-12 15:06:44 -0400

    first commit after connected to github

--- nodeVersion.js
+++ nodeVersion.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const PORT = 3000;
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/version', (req, res, next) => {
-    res.send(process.version + "\n");
+    res.send( {version: `Current version of node: ${process.version}`});
+    //res.send(`Current version of node: ${process.version}`);
 })
 
 app.listen(PORT, () => {
-    //console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
