9cb104b210302740f428eb0ec542163ebfd54203
Nicholas Fafel <nicholasfafel@Nicholass-MBP.home>
2019-06-13 22:42:30 -0400

    fixing server req.body

--- nodeVersion.js
+++ nodeVersion.js
@@ -61,11 +61,8 @@ app.get('/cars/:id', (req, res, next) => {
 });
 
 app.post('/cars', (req, res, next) => {
-    if (!isEmpty(req.query)) {
-        var newCar = req.query;
-    } else if (!isEmpty(req.body)) {
-        var newCar = req.body;
-    }
+    var newCar = req.query;
+    //var newCar = req.body;
     newCar.id = nextId;
     nextId++;
     cars.push(newCar);
