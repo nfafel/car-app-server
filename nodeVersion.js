59579956c114304c18c932f41c8471d42a738e10
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-06-17 12:56:36 -0400

    fixed url

--- nodeVersion.js
+++ nodeVersion.js
@@ -13,7 +13,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 
-MongoClient.connect("mongodb://localhost:27017/MyDb", { useNewUrlParser: true }, function (err, client) {
+MongoClient.connect("mongodb://localhost:27017/myReactAppDb", { useNewUrlParser: true }, function (err, client) {
     var myReactAppDb = client.db('myReactAppDb');
     if(err) throw err;
 
