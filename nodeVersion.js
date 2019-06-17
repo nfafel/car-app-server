6fe69889062df58b938b71571d4ddd0768cd39de
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-06-17 15:12:21 -0400

    connecting to mongodb atlas

--- nodeVersion.js
+++ nodeVersion.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const MONGODB = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const MONGODB = process.env.MONGODB || "mongodb://localhost:27017";
 //const uri = "mongodb+srv://nfafel:Pmwrestling1!@myreactapp-swhip.mongodb.net/test?retryWrites=true&w=majority";
 
 const client = new MongoClient(MONGODB, { useNewUrlParser: true });
