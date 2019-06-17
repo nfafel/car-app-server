8e1e2a2c4134028beb619ca26c1758cedfa901ee
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-06-17 13:44:29 -0400

    fixing connection issue

--- nodeVersion.js
+++ nodeVersion.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, function (err, client) {
+MongoClient.connect("mongodb://localhost:27017", function (err, client) {
     var myReactAppDb = client.db('myReactAppDb');
     if(err) {
         console.log(err);
