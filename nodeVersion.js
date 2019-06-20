4bdede92ddee6f7801843faddf707f6821a207a7
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-06-20 10:22:48 -0400

    fixed problem with process.env.MONGODB

--- nodeVersion.js
+++ nodeVersion.js
@@ -10,9 +10,11 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 
+process.env['MONGODB'] = "mongodb+srv://nfafel:Pmwrestling1!@myreactapp-swhip.mongodb.net/test?retryWrites=true&w=majority";
+
 const PORT = process.env.PORT || 5000;
-//const MONGODB = process.env.MONGODB || "mongodb://localhost:27017";
-const MONGODB = "mongodb+srv://nfafel:Pmwrestling1!@myreactapp-swhip.mongodb.net/test?retryWrites=true&w=majority";
+const MONGODB = process.env.MONGODB || "mongodb://localhost:27017";
+//const MONGODB = "mongodb+srv://nfafel:Pmwrestling1!@myreactapp-swhip.mongodb.net/test?retryWrites=true&w=majority";
 
 MongoClient.connect(MONGODB, { useNewUrlParser: true }, function (err, client) {
     if(err) {
