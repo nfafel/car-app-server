95f706b140efe997cb0ea412b9161ae5bab87169
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-06-24 17:17:55 -0400

    testing autodeploy

--- nodeVersion.js
+++ nodeVersion.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors());
 app.use('/cars', cars);
-app.use('/repairs', repairs)
+app.use('/repairs', repairs);
 
 app.get('/version', (req, res, next) => {
     res.send( {version: `Current version of Node: ${process.version}`} );
