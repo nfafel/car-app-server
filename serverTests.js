23227a4fda7f11304d334dbb27d85c5050e2db61
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-07-02 16:46:54 -0400

    added tests with errors for both the car and repair routes

--- serverTests.js
+++ serverTests.js
@@ -82,6 +82,31 @@ describe('Cars', () => {
     });
 
     /*
+    * Test the /POST route with an error
+    */
+    describe('/POST car', () => {
+        it('it should POST a car with an error', (done) => {
+            let car = {
+                make: "honda",
+                model: "accord",
+                rating: 4
+            }
+            chai.request(server)
+            .post('/cars')
+            .send(car)
+            .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.cars[0].should.have.property('make').eql('honda');
+                    res.body.cars[0].should.have.property('model').eql('accord');
+                    res.body.cars[0].should.have.property('year').eql(2000);
+                    res.body.cars[0].should.have.property('rating').eql(4);
+                done();
+            });
+        });
+    });
+
+    /*
     * Test the /DELETE/:id route
     */
     describe('/DELETE/:id car', () => {
@@ -117,7 +142,31 @@ describe('Cars', () => {
         });
     });
 
+    describe('/PUT/:id car', () => {
+        it('it should UPDATE a car given the id with an error', (done) => {
+            let car = new Cars({make: "acura", model: "TSX", year: 2012, rating: 9})
+            car.save((err, car) => {
+                  chai.request(server)
+                  .put('/cars/' + car.id)
+                  .send({make: "acura", model: "TSX", year: 2015})
+                  .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.cars.should.be.a('array');
+                        res.body.cars[0].should.have.property('year').eql(2015);
+                    done();
+                  });
+            });
+        });
+    });
+
 });
+
+
+/*
+ *
+ * TESTS FOR THE REPAIR ROUTES
+ * 
+ */
 
 
 describe('Repairs', () => {
@@ -154,6 +203,43 @@ describe('Repairs', () => {
                     "make": "lamborghini",
                     "model": "Aventador",
                     "year": 2019,
+                    "rating": 9,
+                    "__v": 0
+                },
+                description: "Cracked Windshield",
+                date: "2019-07-03T00:00:00.000Z",
+                cost: 30000,
+                progress: "completed",
+                technician: "Jerry"
+
+            } );
+        chai.request(server)
+            .post('/repairs')
+            .send(repair)
+            .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.repairs.should.be.a('array');
+                    res.body.repairs[0].car.should.be.a('object');
+                    res.body.repairs[0].should.have.property('description').eql("Cracked Windshield");
+                    res.body.repairs[0].should.have.property('date').eql("2019-07-03T00:00:00.000Z");
+                    res.body.repairs[0].should.have.property('cost').eql(30000);
+                    res.body.repairs[0].should.have.property('progress').eql("completed");
+                    res.body.repairs[0].should.have.property('technician').eql("Jerry");
+                done();
+            });
+        });
+    });
+
+    /*
+    * Test the /POST route with an error
+    */
+    describe('/POST repair', () => {
+        it('it should POST a repair with an error', (done) => {
+            let repair = new Repairs( {
+                car: {
+                    "_id": "5d1baa6a4d4ebc000ff358ef",
+                    "make": "lamborghini",
+                    "model": "Aventador",
                     "rating": 9,
                     "__v": 0
                 },
@@ -257,6 +343,54 @@ describe('Repairs', () => {
                         res.body.repairs[0].should.have.property('description').eql('Engine');
                     done();
                   });
+            });
+        });
+    });
+
+    /*
+    * Test the /PUT route with an error
+    */
+    describe('/PUT/:id repair', () => {
+        it('it should UPDATE a repair given the id with an error', (done) => {
+            let repair = new Repairs( {
+                car: {
+                    "_id": "5d1baa6a4d4ebc000ff358ef",
+                    "make": "lamborghini",
+                    "model": "Aventador",
+                    "year": 2019,
+                    "rating": 9,
+                    "__v": 0
+                },
+                description: "Cracked Windshield",
+                date: "2019-07-03T00:00:00.000Z",
+                cost: 30000,
+                progress: "completed",
+                technician: "Jerry"
+
+            } );
+            repair.save((err, repair) => {
+                chai.request(server)
+                .put('/repairs/' + repair.id)
+                .send({
+                car: {
+                    "_id": "5d1baa6a4d4ebc000ff358ef",
+                    "make": "lamborghini",
+                    "model": "Aventador",
+                    "year": 2019,
+                    "__v": 0
+                },
+                description: "Engine",
+                date: "2019-07-03T00:00:00.000Z",
+                cost: 30000,
+                progress: "completed",
+                technician: "Jerry"
+                })
+                .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.repairs.should.be.a('array');
+                        res.body.repairs[0].should.have.property('description').eql('Engine');
+                    done();
+                });
             });
         });
     });
