c6ffe2600c7652ef3abebcebced0ab2b22d73262
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-07-02 10:46:16 -0400

    added config vars to heroku for MONGODB

--- nodeVersion.js
+++ nodeVersion.js
@@ -9,6 +9,7 @@ const app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
+console.log(process.env.MONGODB);
 //process.env['MONGODB'] = "mongodb+srv://nfafel:Pmwrestling1!@myreactapp-swhip.mongodb.net/myReactAppDb?retryWrites=true&w=majority";
 const MONGODB = process.env.MONGODB || "mongodb://localhost:27017";
 
