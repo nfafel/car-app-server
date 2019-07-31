3fe6cc8de90e7f59ed16e6274291a405c1d15f0a
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-07-31 12:17:52 -0400

    sending response message

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,6 +1,7 @@
 const accountSid = 'ACe5c33365d919ba51826cbbebe1ae8801';
 const authToken = '8f66906e91278f5779413afc931ce205';
 const client = require('twilio')(accountSid, authToken);
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 exports.sendMessage = async(req, res) => {
     var text;
@@ -17,6 +18,9 @@ exports.sendMessage = async(req, res) => {
             to: `+${req.params.number}`
         });
         res.send(message.sid);
+
+        const twiml = new MessagingResponse();
+        twiml.message('response test');
 
     } catch(err) {
         console.log(err);
