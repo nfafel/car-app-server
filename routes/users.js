251578eb8b5b2edb390e5000958c1dd6d4568d83
Nicholas Fafel <nicholasfafel@Nicholass-MBP.hsd1.pa.comcast.net>
2019-08-12 21:51:06 -0400

    changed login user route to post route

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,13 @@ const usersRouter = express.Router();
 
 const users_controller = require('../controllers/users');
 
-usersRouter.get('/login', users_controller.loginUser);
-
 usersRouter.get('/availability', users_controller.checkAvailability);
 
 usersRouter.put('/changeSubscription', users_controller.changeSubscription)
 
 usersRouter.post('/', users_controller.post);
+
+usersRouter.post('/login', users_controller.loginUser);
 
 usersRouter.delete('/', users_controller.delete);
 
