dd7ad9ebda5c8233dc97ec88cd21d82c6f6ad266
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-08-14 14:15:16 -0400

    changed token to variable

--- graphQL/context.js
+++ graphQL/context.js
@@ -3,17 +3,16 @@ const jwt = require('jsonwebtoken');
 const Users = require('../models/users');
 
 exports.contextFunc = async( req ) => {
-    console.log(req)
-    // try {
-    //     const token = req..authorization.split(" ")[1];
-    //     console.log(token);
-    //     const decoded = jwt.decode(token);
-    //     const phoneNumber = decoded.payload.phoneNumber;
-    //     const user = await Users.findOne({phoneNumber: phoneNumber});
-    //     const verified = jwt.verify(token, user.secret);
-    //     return verified.payload;
+    try {
+        const token = req.body.variables.authorization.split(" ")[1];
+        console.log(token);
+        const decoded = jwt.decode(token);
+        const phoneNumber = decoded.payload.phoneNumber;
+        const user = await Users.findOne({phoneNumber: phoneNumber});
+        const verified = jwt.verify(token, user.secret);
+        return verified.payload;
 
-    // } catch(err) {
-    //     return {authenticationError: err}
-    // }
+    } catch(err) {
+        return {authenticationError: err}
+    }
 }
