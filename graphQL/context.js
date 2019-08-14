904b0518fbb3917b53f1c2919e6d7997f73b5490
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-08-14 13:29:51 -0400

    testing

--- graphQL/context.js
+++ graphQL/context.js
@@ -5,6 +5,7 @@ const Users = require('../models/users');
 exports.contextFunc = async( req ) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
         const decoded = jwt.decode(token);
         const phoneNumber = decoded.payload.phoneNumber;
         const user = await Users.findOne({phoneNumber: phoneNumber});
