94807ae4a7a4198af9a90f0865f34edd0e6d6ba9
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-08-15 08:31:11 -0400

    added authentication to changeSubscription route

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const usersRouter = express.Router();
 
 const users_controller = require('../controllers/users');
+const verifyJWT_controller = require('../controllers/verifyJWT');
 
 usersRouter.get('/:phoneNumber/availability', users_controller.checkAvailability);
 
-usersRouter.put('/changeSubscription', users_controller.changeSubscription)
+usersRouter.put('/changeSubscription', verifyJWT_controller.verifyJWT, users_controller.changeSubscription)
 
 usersRouter.post('/', users_controller.post);
 
