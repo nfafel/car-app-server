7704d275d18757fb4c2b84428b6a32d3f80d9496
Nicholas Fafel <nicholasfafel@Nicholass-MacBook-Pro.local>
2019-08-19 13:01:52 -0400

    updated context function

--- graphQL/context.js
+++ graphQL/context.js
@@ -3,14 +3,19 @@ const jwt = require('jsonwebtoken');
 const Users = require('../models/users');
 
 exports.contextFunc = async( req ) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.decode(token);
-        const phoneNumber = decoded.payload.phoneNumber;
-        const user = await Users.findOne({phoneNumber: phoneNumber});
-        const verified = jwt.verify(token, user.secret);
-        return verified.payload;
-    } catch(err) {
-        return {authenticationError: err}
-    }
+    if (req.headers.authorization !== null) {
+        try {
+            const token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.decode(token);
+            const phoneNumber = decoded.payload.phoneNumber;
+            const user = await Users.findOne({phoneNumber: phoneNumber});
+            const verified = jwt.verify(token, user.secret);
+            var context = verified.payload;
+            context.authenticationVerified = true;
+            return context;
+        } catch(err) {
+            return {authenticationVerified: false}
+        }
+    } 
+    return {authenticationVerified: false}
 }
